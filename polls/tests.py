d77d14ac9e697149c40690171f76dfc45d03ba48
dimaseg <s41.blizzard@mail.ru>
2018-02-23 15:31:56 +0300

    test file

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,3 @@
-from django.test import TestCase
-
 # Create your tests here.
 import datetime
 from django.urls import reverse
@@ -7,6 +5,28 @@ from django.utils import timezone
 from django.test import TestCase
 
 from .models import Question
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
 
 
 class QuestionModelTests(TestCase):
